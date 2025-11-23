4349c6529bd525cfa1719ad091dbce3028291dc0
Monique Gomes <niquevellin@gmail.com>
2025-11-23 10:00:56 -0300

    alterações para receber imagem do mapa do backend

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -16,19 +16,25 @@ graph_instance.load_from_matrix(default_matrix)
 #----------------------------------------------------endpoints
 @router.get("/graph")
 def get_graph():
-    return graph_instance.to_dict()
+    return graph_instance.to_dict(path=None)
 
 @router.post("/update-map")
 def update_map(data: MatrixInput):
     try:
         graph_instance.load_from_matrix(data.matrix_text)
-        return {"status": "ok", "graph": graph_instance.to_dict()}
+        return {"status": "ok", "graph": graph_instance.to_dict(path=None)}
     except Exception as e:
         return {"status": "error", "message": str(e)}
     
 @router.post("/calculate")
 def calculate_route(data: RouteInput):
-    return graph_instance.get_shortest_path(data.start, data.end)
+    result = graph_instance.get_shortest_path(data.start, data.end)
+    path_to_highlight = result["path"] if result["distance"] != -1 else None
+
+    return {
+        "result": result,
+        "graph": graph_instance.to_dict(path=path_to_highlight)
+    }
 
 @router.post("/random-map")
 def generate_random_map():
@@ -48,4 +54,4 @@ def generate_random_map():
     matrix_text = ";".join(matrix_rows)
     graph_instance.load_from_matrix(matrix_text)
 
-    return {"status": "ok", "graph": graph_instance.to_dict()}
+    return {"status": "ok", "graph": graph_instance.to_dict(path=None)}
