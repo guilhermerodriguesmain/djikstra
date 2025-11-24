91dd1ffe486e6f1b989ae436356c9183a1b07631
Guilherme <guilhermerodrigues9369@gmail.com>
2025-11-23 21:10:43 -0300

    refator: cleancode + boas praticas + melhoria método random_edges

--- backend/app/core/graph.py
+++ backend/app/core/graph.py
@@ -46,17 +46,37 @@ class Graph:
 
         return distances[end], path
     
-    def insert_random_edges(self):
-        letras = str.ascii_uppercase
-        for _ in range(1,7):
-            if len(self.node_list) == 0:
-                self.node_list.append((rnd.choice(letras), rnd.choice(letras), rnd.randint(1,10)))
-                self.G.add_weighted_edges_from(self.node_list)
-            else:
-                self.node_list.append(((self.node_list[-1][1]), rnd.choice(letras), rnd.randint(1,10)))
-                self.G.add_weighted_edges_from(self.node_list)
-        return self.adj
-    
+    def random_edges(self,G):
+        edges = []
+
+        # Se o grafo não tem nós, cria dois
+        if len(self.G.nodes) < 1:
+            a = rnd.choice(str.ascii_uppercase)
+            b = rnd.choice(str.ascii_uppercase)
+            if a == b:
+                b = rnd.choice(str.ascii_uppercase)
+            w = rnd.randint(1, 30)
+            edges.append((a, b, {'weight': w}))
+
+        # Agora gera várias novas arestas
+        for _ in range(rnd.randint(5, 10)):
+            a = edges[-1][1]
+            b = rnd.choice(str.ascii_uppercase)
+            if a == b:
+                b = rnd.choice(str.ascii_uppercase)
+            w = rnd.randint(1, 30)
+            edges.append((a, b, {'weight': w}))
+            
+        # adiciona arestas entre nós já existentes   
+            c = edges[-1][1]
+            d = edges[rnd.randint(0, len(edges)-2)][1]
+            if a == b:
+                edges[rnd.randint(0, len(edges)-3)][1]
+            w = rnd.randint(1, 30)
+            edges.append((c, d, {'weight': w}))
+
+        self.G.add_edges_from(edges)
+        return self.G.edges(data=True)
     def manual_insert_edges(self, edges):
         self.node_list.append(((self.node_list[-1][1]), edges, rnd.randint(1,25)))
         self.G.add_edge(self.node_list)
